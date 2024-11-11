3fcbcb9d5365743512559659d96899df23b0a345
bogda <bogdan.semenyuk11@gmail.com>
2024-11-11 16:00:54 +0200

    ok

---
+++ dz6_1.py
@@ -0,0 +1,6 @@
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+number3 = float(input("Enter third number: "))
+
+result = number1 * number2 * number3
+print("result multiplication of three numbers", result)
