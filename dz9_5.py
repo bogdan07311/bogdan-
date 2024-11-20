8ce8f1d0e761f145e6b09cc4fe89c73b8de00e4d
bogda <bogdan.semenyuk11@gmail.com>
2024-11-20 16:21:18 +0200

    ok

---
+++ dz9_5.py
@@ -0,0 +1,27 @@
+class programminglanguage:
+    def __init__(self, name):
+        self.name = name
+
+    '''def output_greetings(self):
+        print(f"Hello! You are working with a programming language {self.name}!")
+
+class Python(programminglanguage):
+    def __init__(self):
+        super().__init__("Python")
+
+class Java(programminglanguage):
+    def __init__(self):
+        super().__init__("Java")
+
+class CPlusPlus(programminglanguage):
+    def __init__(self):
+        super().__init__("C++")
+
+python = Python()
+java = Java()
+cpp = CPlusPlus()
+python.output_greetings()
+java.output_greetings()
+cpp.output_greetings()'''
+
+
